db6feb5e8370a73faf8d7f6d6d066763b077a443
jbblanchet <jbblanchet@dashthis.com>
2014-12-15 09:13:59 -0500

    Code review changes

--- silent-reporter.js
+++ silent-reporter.js
@@ -3,16 +3,16 @@ var gutil = require('gulp-util');
 module.exports = function(done) {
 	var failureCount = 0;
 
+	this.specDone = function(result) {
+		if (result.status === 'failed') {
+			failureCount++;
+		}
+	};
+	
 	this.jasmineDone = function() {
 		var result = failureCount === 0 ? null : new gutil.PluginError('gulp-jasmine', 'Tests failed', {
 			showStack: false
 		});
 		done(result);
-	};
-
-	this.specDone = function(result) {
-		if (result.status === 'failed') {
-			failureCount++;
-		}
 	};
 };
