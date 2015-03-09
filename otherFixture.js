e513689fa4dc9a7b3c6c3dbe0e9b5c436947fd5f
Josh Young <jyoung@vecna.com>
2015-03-09 13:31:31 -0400

    Allowing parallel streams to not prevent test from running from other streams.

---
+++ otherFixture.js
@@ -0,0 +1,6 @@
+describe('other fixture', function () {
+	it('should pass another test', function (done) {
+		expect(1).toBe(1);
+		done();
+	});
+});
--- test.js
+++ test.js
@@ -57,3 +57,50 @@ it('should run the test only once even if called in succession', function (done)
 
 	stream.end();
 });
+
+it('should run all tests when new tests are added while running tests.', function (done) {
+	var numTasks = 0;
+	var output = '';
+	var oldWrite = process.stdout.write;
+	process.stdout.write = function (str) {
+		output += str;
+	};
+	var startStream = function() {
+		numTasks++;
+		var stream = jasmine({
+			timeout: 9000,
+			verbose: true
+		});
+		var reader = through2.obj(function (file, enc, cb) {
+			cb();
+		}, function (cb) {
+			process.stdout.write = out;
+			assert.equal(output.match(/should pass another test: passed/g).length, 1);
+			if (--numTasks <= 0) {
+				done();
+			}
+			cb();
+		});
+
+		stream.pipe(reader);
+
+		return stream;
+	};
+
+	var otherFixtureStream = startStream();
+	var fixtureStream = startStream();
+
+
+	fixtureStream.write(new gutil.File({
+		path: 'fixture.js',
+		contents: new Buffer('')
+	}));
+
+	otherFixtureStream.write(new gutil.File({
+		path: 'otherFixture.js',
+		contents: new Buffer('')
+	}));
+
+	fixtureStream.end();
+	otherFixtureStream.end();
+});
