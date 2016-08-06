774715da0616a22d76aaffdec5e67f2c7ffca070
Maciej Chałapuk <maciej@chalapuk.pl>
2016-08-06 23:33:01 +0200

    Added a test for all tests being done on 'end' event.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 'use strict';
+
 var gulp = require('gulp');
 var jasmine = require('./');
+var flag = require('./flag');
 
 gulp.task('default', function () {
-	return gulp.src('fixture.js').pipe(jasmine({
-		timeout: 10000
-	}));
+	return gulp.src('fixture.js')
+		.pipe(jasmine({	timeout: 10000 }))
+		.on('end', checkTestsDone)
+	;
 });
+
+function checkTestsDone() {
+	if (!flag.testsDone) {
+		throw new Error('jasmine should not be running after \'end\' event');
+	}
+}
+
